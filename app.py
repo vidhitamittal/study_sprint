d60d94ba644393d2a6ec1037da53287006b74178
vidhita <122602734+vidhitamittal@users.noreply.github.com>
2026-01-14 11:04:19 +0530

    smal change

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import re
 
-genai.configure(api_key="AIzaSyAMuNAtfBC-9fwqiylyqPLN8iO6KR2HJbU") # will need to be replaced with your actual API key
+genai.configure(api_key="yourAPIkey") # will need to be replaced with your actual API key
 model = genai.GenerativeModel('gemini-3-flash-preview') # use the appropriate google gen ai model
 
 st.set_page_config(page_title="AI Study Buddy", page_icon="🎓", layout="wide")
